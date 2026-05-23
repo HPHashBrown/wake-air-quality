fa335c8688b56b2e574a7b3b18e5c078f1ec6638
HPHashBrown <163060144+HPHashBrown@users.noreply.github.com>
2026-05-23 14:37:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ else:
 st.markdown('<p class="title-gradient">Project Wake AQI</p>', unsafe_allow_html=True)
 st.markdown('<p class="sub-font">Statewide Atmospheric PM2.5 Analytics Engine.</p>', unsafe_allow_html=True)
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["📊 Telemetry & Forecasting", "🧠 Predictive Scenario Core", "🩺 Health Literacy", "🛰️ Statewide Vector Map", "🌌NASA Space Intelligence", "🚲 The Clean-Air Commute"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["📊 Telemetry & Forecasting", "🧠 Predictive Scenario Core", "🩺 Health Literacy", "🛰️ Statewide Vector Map", "🌌NASA Space Intelligence", "🚲 The Clean-Air Commute"])
 
 # --- TAB 1: OVERVIEW ---
 
