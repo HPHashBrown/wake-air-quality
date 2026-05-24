14f2217299c6ab631eb9df57508e63c6e11edac8
HPHashBrown <163060144+HPHashBrown@users.noreply.github.com>
2026-05-24 16:44:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ ox.settings.timeout = 300
 # Tell OSMnx to use the public Overpass server via a more stable request method
 ox.settings.use_cache = True
 
-st.set_page_config(page_title="Wake AQI", page_icon="🌐", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Project AIR", page_icon="🌐", layout="wide", initial_sidebar_state="expanded")
 st_autorefresh(interval=600000, key="datarefresh")
 
 if 'map_center' not in st.session_state:
