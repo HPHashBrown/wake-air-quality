5b448b7b73d6f1668ea9b5c7f65772da51c5b2c2
HPHashBrown <163060144+HPHashBrown@users.noreply.github.com>
2026-05-26 08:33:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ import openaq
 import pydeck as pdk
 import random
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2.service_account import ServiceAccountCredentials
 
 
